a2b36281bb7d2e3deff2a6667bebec1a7b987a5c
Shathi paul <108425880+shathipaul@users.noreply.github.com>
2023-01-10 00:48:49 +0600

    first commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HeroLeft from './HeroLeft';
+import HeroRight from './HeroRight';
+
+const Hero = () => {
+    return (
+        <div className="hero absolute-center">
+            <div className="hero-content flex-col lg:flex-row justify-between">
+                <HeroLeft />
+                <div className='max-w-lg ml-7'>
+                    <HeroRight />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
